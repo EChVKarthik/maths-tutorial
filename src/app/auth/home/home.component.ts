d1049610d4e2dfa2f63af6df35dd34d19a9b7ab8
EChVKarthik <e.ch.v.karthik@gmail.com>
2020-07-04 08:40:15 +0530

    fourth commit

--- src/app/auth/home/home.component.ts
+++ src/app/auth/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service'
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: LoginService) { }
+
+  email: string;
+password: string;
+
+  signUp(){
+    this.service.signup(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+  signIn() {
+    this.service.signin(this.email, this.password);
+    this.email = '';
+    this.password = '';
+  }
+
+  signOut() {
+    this.service.signout();
+  }
 
   ngOnInit(): void {
   }
