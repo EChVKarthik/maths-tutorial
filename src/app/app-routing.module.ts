acb1ac8933344887c2182925ab143add8a468510
EChVKarthik <e.ch.v.karthik@gmail.com>
2020-07-04 07:36:18 +0530

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { AboutComponent } from './auth/about/about.component';
+import { GeometryComponent } from './auth/geometry/geometry.component'
+import { HomeComponent } from './auth/home/home.component';
+const routes: Routes = [
+  {path:'',component: HomeComponent},
+  {path:'about_us',component: AboutComponent},
+  {path: 'geometry',component: GeometryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  AboutComponent, 
+  GeometryComponent
+]
